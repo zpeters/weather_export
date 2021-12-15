ed99794095e080fdcb7fea9e622bf77a7171c054
Zach Peters <zpeters@gmail.com>
2021-12-14 20:25:55 -0600

    Basic updates

--- main.go
+++ main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
-        "fmt"
-        "os"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 var apiKey = os.Getenv("OPEN_WEATHER_API_KEY")
+var zipCode = "39110"
+
+type MainWeather struct {
+	TempF    float32 `json:"temp"`
+	TempFMax float32 `json:"temp_max"`
+	TempFMin float32 `json:"temp_min"`
+	Humidity float32 `json:"humidity"`
+}
+
+type Weather struct {
+	Name string      `json:"name"`
+	DT   int         `json:"dt"`
+	Main MainWeather `json:"main"`
+}
 
 func main() {
-        fmt.Println(apiKey)
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=%s,us&appid=%s", zipCode, apiKey)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%s\n", body)
+
+	var weather Weather
+	json.Unmarshal(body, &weather)
+	fmt.Printf("%#v\n", weather)
 }
